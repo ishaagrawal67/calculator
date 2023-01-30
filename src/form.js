98f34a464c5784afa77f67c051c8930b20b7f364
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2023-01-30 14:23:30 +0530

    calculator

---
+++ src/form.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function form(){
+    return(
+        <form className="container">
+            <div className='header'>
+                <h1>Registration form</h1>
+            </div>
+        </form>
+
+
+    );
+}
+export default form
